a2cda95ac2ecb4dc21043fb956c9d8576f6c4acd
Thao Nessround <tnessrou@ge-b1.kzn.21-school.ru>
2021-06-02 17:07:55 +0300

    conversions completed

--- main.c
+++ main.c
@@ -11,10 +11,15 @@ int	main(void)
 		//  "Ilya", 20, 'm');
 	// printf("My name is %s, I am %d years old. My favorite letter is '%c' %%\n",
 		//  "Ilya", 20, 'm');
-	int a = 12;
-	void *p = &a;
-	printf("%p\n", p);
-	printf("%s%i%c\n", "aaa", 1123444434, 64);
-	ft_printf("%s%i%c\n", "aaa", 1123444434, 64);
+	// int a = 12;
+	// void *p = &a;
+	// printf("%p\n", p);
+	// ft_printf("%p\n", p);
+	printf("%s%X%c\n", "aaa", 1123444434, 64);
+	ft_printf("%s%X%c\n", "aaa", 1123444434, 64);
+	printf("%s%o%c\n", "aaa", 1123444434, 64);
+	ft_printf("%s%o%c\n", "aaa", 1123444434, 64);
+	printf("%s%u%c\n", "aaa", 32332, 64);
+	ft_printf("%s%u%c\n", "aaa", 32332, 64);
 	return (0);
 }
